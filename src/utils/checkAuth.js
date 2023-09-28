cc7046ea43f919dfbec858e03e6401759286137f
Artem Ibragimov <aribragimov@inbox.ru>
2023-09-28 13:51:57 +0300

    rename error message

--- src/utils/checkAuth.js
+++ src/utils/checkAuth.js
@@ -2,20 +2,14 @@ import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
     const token = (req.headers.authorization || '').split(' ')[1]
-    if (token) {
-        try {
+    try {
 
-            const decoded = jwt.verify(token, process.env.SECRET_KEY)
-            req.id = decoded.id
-            next()
-        } catch (err) {
-            return res.status(403).json({
-                message: 'Нет доступа'
-            })
-        }
-    } else {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        req.id = decoded.id
+        next()
+    } catch (err) {
         return res.status(403).json({
-            message: 'Нет доступа'
+            message: 'No access'
         })
     }
 }
