508c3c039a09e9722030eed9928766d797e3e526
Artem Ibragimov <aribragimov@inbox.ru>
2023-10-02 11:38:08 +0300

    fix bug

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import cors from "cors";
 import { routes } from "./routes/routes.js";
 
 const app = express();
-const port = process.env.PORT3001;
-const domain = process.env.DOMAIN;
 app.use(cors());
 app.use(express.json());
-
 app.use("/", routes);
 
-app.listen(port, () => {
-  console.log("Server started on " + domain + ":" + port);
+app.listen(process.env.PORT3001, () => {
+  console.log(
+    "Started " + process.env.DOMAIN + ":" + process.env.PORT3001 + "/"
+  );
 });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,47 +1,47 @@
-import express, {Router} from "express";
+import express, { Router } from "express";
 import checkAuth from "../utils/checkAuth.js";
 import {
-    loginValidations,
-    registerValidations,
+  loginValidations,
+  registerValidations,
 } from "../validations/validations.js";
 import handleValidationErrors from "../utils/handleValidationErrors.js";
-import {UserController} from "../controllers/index.js";
+import { UserController } from "../controllers/index.js";
 import multer from "multer";
 
 export const userRoutes = Router();
 
 userRoutes.use(
-    "/uploads",
-    express.static("C:\\CODE\\todo-server\\src\\uploads")
+  "/uploads",
+  express.static("C:\\CODE\\todo-server\\src\\uploads")
 );
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "C:\\CODE\\todo-server\\src\\uploads");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "C:\\CODE\\todo-server\\src\\uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 userRoutes.post(
-    "/uploads",
-    checkAuth,
-    upload.single("image"),
-    UserController.uploadAvatar
+  "/uploads",
+  checkAuth,
+  upload.single("image"),
+  UserController.uploadAvatar
 );
 
 userRoutes.post(
-    "/signup",
-    registerValidations,
-    handleValidationErrors,
-    UserController.register
+  "/signup",
+  registerValidations,
+  handleValidationErrors,
+  UserController.register
 );
 userRoutes.post(
-    "/login",
-    loginValidations,
-    handleValidationErrors,
-    UserController.login
+  "/login",
+  loginValidations,
+  handleValidationErrors,
+  UserController.login
 );
 
 userRoutes.get("/me", checkAuth, UserController.me);
