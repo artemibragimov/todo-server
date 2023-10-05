1f7e9f8aacd2f165c946a5b354123ac31ae83ccc
Artem Ibragimov <aribragimov@inbox.ru>
2023-10-05 16:19:09 +0300

    add cookie

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes/index.js';
 import { env } from './utils/helper.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
 app.use('/', routes);
 
 //error middleware
